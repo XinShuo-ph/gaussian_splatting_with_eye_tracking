fixed memory leakage problems

fps_test_amr_1080p.py
@@ -4,8 +4,7 @@ from scene import Scene
 import os
 from tqdm import tqdm
 from os import makedirs
-from gaussian_renderer_amr import render
-import torchvision
+from gaussian_renderer_amr import render_once
 from utils.general_utils import safe_state
 from argparse import ArgumentParser
 from arguments import ModelParams, PipelineParams, get_combined_args
@@ -53,7 +52,7 @@ for ratio in [1]:
     for idx, view in enumerate(tqdm(views, desc="Rendering progress")):
         time = 0
         for i in range(5):
-            rendering = render(view, gaussians, pipeline, background,starter = starter, ender= ender)["render"]
+            rendering = render_once(view, gaussians, pipeline, background,starter = starter, ender= ender)["render"]
             torch.cuda.synchronize()
             time += starter.elapsed_time(ender)
         # count fps every 5 frames

fps_test_amr_foveated.py
@@ -11,6 +11,7 @@ from argparse import ArgumentParser
 from arguments import ModelParams, PipelineParams, get_combined_args
 from gaussian_renderer_amr import GaussianModel
 
+
 pix_x = 1920
 pix_y = 1080
 
@@ -19,11 +20,7 @@ parser = ArgumentParser(description="Testing script parameters")
 model = ModelParams(parser, sentinel=True)
 pipeline = PipelineParams(parser)
 
-print(pipeline.debug)
-
 # pipeline.debug = True
-
-print(pipeline.debug)
 
 parser.add_argument("--iteration", default=-1, type=int)
 parser.add_argument("--skip_train", action="store_true")
@@ -34,17 +31,44 @@ print("Rendering " + args.model_path)
 safe_state(args.quiet)
 mydataset = model.extract(args)
 
-# render the acurate image for reference
+
 gaussians = GaussianModel(mydataset.sh_degree) # create an empty gaussian model
 scene = Scene(mydataset, gaussians, load_iteration=args.iteration, shuffle=False) # load the model and cameras
 views = scene.getTrainCameras()
-views = [views[i] for i in range(251)] # use 100 views to average the fps
+views = [views[i] for i in range(100)] # use 100 views to average the fps
 bg_color = [1,1,1] if mydataset.white_background else [0, 0, 0]
 background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
 
+
+
 pix_horizon = []
 fps_avg = []
-for ratio in [1]:
+fps_avg0 = []
+fps_avg1 = []
+fps_avg2 = []
+fps_avg3 = []
+for ratio in np.linspace(0.2,2.0,10):
+
+    # restart cuda kernel and reload the model to avoid memory leak
+    # torch.cuda.reset_max_memory_allocated()
+    # torch.cuda.reset_peak_memory_stats()
+    
+    print(f"GPU memory allocated: {torch.cuda.memory_allocated() / 1e9:.2f} GB")
+    print(f"GPU memory reserved: {torch.cuda.memory_reserved() / 1e9:.2f} GB")
+
+    # if 'gaussians' in locals():
+    #     del gaussians
+    # if 'scene' in locals():
+    #     del scene
+    # if 'views' in locals():
+    #     del views
+    # if 'background' in locals():
+    #     del background
+    # if 'result' in locals():
+    #     del result
+    # if 'rendering' in locals():
+    #     del rendering
+
 
     print(f"Rendering at {int(pix_x*ratio)}x{int(pix_y*ratio)}")
     # change the image width and height
@@ -76,9 +100,10 @@ for ratio in [1]:
         time2 = 0
         time3 = 0
         for i in range(5):
-            rendering = render(view, gaussians, pipeline, background,starter = starter, ender= ender, 
+            result = render(view, gaussians, pipeline, background,starter = starter, ender= ender, 
                                starters = [starter0, starter1, starter2, starter3], enders = [ender0, ender1, ender2, ender3]
-                               )["render"]
+            )
+            rendering = result["render"]
             torch.cuda.synchronize()
             time += starter.elapsed_time(ender)
             time0 += starter0.elapsed_time(ender0)
@@ -113,3 +138,55 @@ for ratio in [1]:
 
     pix_horizon.append(int(pix_x*ratio))
     fps_avg.append(avg_fps)
+    fps_avg0.append(avg_fps0)
+    fps_avg1.append(avg_fps1)
+    fps_avg2.append(avg_fps2)
+    fps_avg3.append(avg_fps3)
+
+
+    
+# plot the fps vs horizontal pix curve for all steps
+import matplotlib.pyplot as plt
+plt.plot(pix_horizon, fps_avg, 'ko-',label = 'total')
+plt.plot(pix_horizon, fps_avg0, 'ro-',label = 'preprocess + level 1')
+plt.plot(pix_horizon, fps_avg1, 'go-',label = 'level 2')
+plt.plot(pix_horizon, fps_avg2, 'bo-',label = 'level 3')
+plt.plot(pix_horizon, fps_avg3, 'yo-',label = 'level 4')
+plt.xlabel('Horizontal Pixels')
+plt.ylabel('FPS')
+plt.title('model: 3DGS_AMR, machine: T4')
+plt.legend()
+plt.savefig('fps_benchmark_amr_fovsteps.png')
+
+plt.figure()
+plt.plot(pix_horizon, fps_avg, 'ko-',label = 'total')
+plt.plot(pix_horizon, fps_avg0, 'ro-',label = 'preprocess + level 1')
+plt.plot(pix_horizon, fps_avg1, 'go-',label = 'level 2')
+plt.plot(pix_horizon, fps_avg2, 'bo-',label = 'level 3')
+plt.plot(pix_horizon, fps_avg3, 'yo-',label = 'level 4')
+plt.plot(np.linspace(800,4000,100), 1.5e8/np.linspace(800,4000,100)**2, 'k--',label = '$y\\propto x^{-2}$')
+plt.plot(np.linspace(800,4000,100), 7e4/np.linspace(800,4000,100), 'b--',label = '$y\\propto x^{-1}$')
+plt.xlabel('Horizontal Pixels')
+plt.ylabel('FPS')
+plt.title('model: 3DGS_AMR, machine: T4')
+plt.legend()
+plt.ylim(1,120)
+plt.savefig('fps_benchmark_scale_amr_fovsteps.png')
+
+
+plt.figure()
+plt.plot(pix_horizon, fps_avg, 'ko-',label = 'total')
+plt.plot(pix_horizon, fps_avg0, 'ro-',label = 'preprocess + level 1')
+plt.plot(pix_horizon, fps_avg1, 'go-',label = 'level 2')
+plt.plot(pix_horizon, fps_avg2, 'bo-',label = 'level 3')
+plt.plot(pix_horizon, fps_avg3, 'yo-',label = 'level 4')
+plt.plot(np.linspace(800,4000,100), 1.8e8/np.linspace(800,4000,100)**2, 'k--',label = '$y\\propto x^{-2}$')
+plt.plot(np.linspace(800,4000,100), 7e4/np.linspace(800,4000,100), 'b--',label = '$y\\propto x^{-1}$')
+plt.semilogy(np.linspace(800,4000,100), 140*np.exp( - 0.75* np.linspace(800,4000,100)/1000), 'y--',label = '$\\log y \\propto C - x $')
+plt.xlabel('Horizontal Pixels')
+plt.ylabel('FPS')
+plt.title('model: 3DGS_AMR, machine: T4')
+plt.legend()
+plt.ylim(10,120)
+plt.savefig('fps_benchmark_log_amr_fovsteps.png')
+

gaussian_renderer_amr/__init__.py
@@ -49,7 +49,7 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
         debug=pipe.debug
     )
 
-    print(raster_settings.debug)
+    # print(raster_settings.debug)
 
     rasterizer = GaussianRasterizer(raster_settings=raster_settings)
 
@@ -100,13 +100,23 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
     # I record times here because the color and cov are not computed in python when testing fps
     # loading gaussian model parameters in python took similar time as rendering 
     # this is also what fov-3DGS does: Fov-3DGS/fov3dgs/gaussian_renderer_fov_mmfr/__init__.py:72
-
-    print(" fovea step 0 ")
+    if pipe.debug:
+        print(" fovea step 0 ")
     if starter is not None:
         starter.record()
 
     if starters is not None:
         starters[0].record()
+
+
+    radii = torch.empty(0, dtype=torch.int32, device='cuda')
+    parsed_point_list = torch.empty(0, dtype=torch.int32, device='cuda')
+    parsed_ranges = torch.empty(0, dtype=torch.int32, device='cuda')
+    parsed_tile_AMR_levels = torch.empty(0, dtype=torch.int32, device='cuda')
+    geomBuffer = torch.empty(0, dtype=torch.int8, device='cuda')
+    binningBuffer = torch.empty(0, dtype=torch.int8, device='cuda')
+    imgBuffer = torch.empty(0, dtype=torch.int8, device='cuda')
+
     
 
     # Rasterize visible Gaussians to image, obtain their radii (on screen). 
@@ -138,19 +148,35 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
     
     if enders is not None:
         enders[0].record()
-    
+
+    # radii = radii.to(torch.int32)
+    # parsed_point_list = parsed_point_list.to(torch.int32)
+    # parsed_ranges = parsed_ranges.to(torch.int32)
+    # parsed_tile_AMR_levels = parsed_tile_AMR_levels.to(torch.int32)
+
+    # check all the dtypes that should be int
+    if pipe.debug:
+        print("radii dtype: ", radii.dtype)
+        print("parsed_point_list dtype: ", parsed_point_list.dtype)
+        print("parsed_ranges dtype: ", parsed_ranges.dtype)
+        print("parsed_tile_AMR_levels dtype: ", parsed_tile_AMR_levels.dtype)
+        print("geomBuffer dtype: ", geomBuffer.dtype)
+        print("binningBuffer dtype: ", binningBuffer.dtype)
+        print("imgBuffer dtype: ", imgBuffer.dtype)
+
     # make a deep copy of the torch tensor parsed_tile_AMR_levels and reduce by 3 (if <1, floor to 1) to get the AMR levels of step 0
-    parsed_tile_AMR_levels_step0 = parsed_tile_AMR_levels.clone()
-    parsed_tile_AMR_levels_step0 -= 3
-    parsed_tile_AMR_levels_step0[parsed_tile_AMR_levels_step0 < 1] = 1
-    # similarly for step 1, except -=2
-    parsed_tile_AMR_levels_step1 = parsed_tile_AMR_levels.clone()
-    parsed_tile_AMR_levels_step1 -= 2
-    parsed_tile_AMR_levels_step1[parsed_tile_AMR_levels_step1 < 1] = 1
+    # parsed_tile_AMR_levels_step0 = parsed_tile_AMR_levels.clone()
+    # parsed_tile_AMR_levels_step0 -= 3
+    # parsed_tile_AMR_levels_step0[parsed_tile_AMR_levels_step0 < 1] = 1
+    # # similarly for step 1, except -=2
+    # parsed_tile_AMR_levels_step1 = parsed_tile_AMR_levels.clone()
+    # parsed_tile_AMR_levels_step1 -= 2
+    # parsed_tile_AMR_levels_step1[parsed_tile_AMR_levels_step1 < 1] = 1
     # TODO: if ourside the current fovea, set to same as last step
 
 
-    print(" fovea step 1 ")
+    if pipe.debug:
+        print(" fovea step 1 ")
 
     if starters is not None:
         starters[1].record()
@@ -174,8 +200,8 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
         geom_rgb_precomp = parsed_geom_rgb,
         point_list_precomp = parsed_point_list,
         ranges_precomp = parsed_ranges,
-        tile_AMR_levels_last = parsed_tile_AMR_levels_step0,
-        tile_AMR_levels_current = parsed_tile_AMR_levels_step1,
+        # tile_AMR_levels_last = parsed_tile_AMR_levels_step0,
+        # tile_AMR_levels_current = parsed_tile_AMR_levels_step1,
         geomBuffer_precomp = geomBuffer,
         binningBuffer_precomp = binningBuffer,
         imageBuffer_precomp = imgBuffer
@@ -185,11 +211,12 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
         enders[1].record()
         
     # similarly for step 2, except -=1
-    parsed_tile_AMR_levels_step2 = parsed_tile_AMR_levels.clone()
-    parsed_tile_AMR_levels_step2 -= 1
-    parsed_tile_AMR_levels_step2[parsed_tile_AMR_levels_step2 < 1] = 1
+    # parsed_tile_AMR_levels_step2 = parsed_tile_AMR_levels.clone()
+    # parsed_tile_AMR_levels_step2 -= 1
+    # parsed_tile_AMR_levels_step2[parsed_tile_AMR_levels_step2 < 1] = 1
 
-    print(" fovea step 2 ")
+    if pipe.debug:
+        print(" fovea step 2 ")
 
     if starters is not None:
         starters[2].record()
@@ -211,8 +238,8 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
         geom_rgb_precomp = parsed_geom_rgb,
         point_list_precomp = parsed_point_list,
         ranges_precomp = parsed_ranges,
-        tile_AMR_levels_last = parsed_tile_AMR_levels_step1,
-        tile_AMR_levels_current = parsed_tile_AMR_levels_step2,
+        # tile_AMR_levels_last = parsed_tile_AMR_levels_step1,
+        # tile_AMR_levels_current = parsed_tile_AMR_levels_step2,
         geomBuffer_precomp = geomBuffer1,
         binningBuffer_precomp = binningBuffer1,
         imageBuffer_precomp = imgBuffer1
@@ -222,9 +249,10 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
         enders[2].record()
 
     # step 3 is the same as parsed_tile_AMR_levels
-    parsed_tile_AMR_levels_step3 = parsed_tile_AMR_levels.clone()
+    # parsed_tile_AMR_levels_step3 = parsed_tile_AMR_levels.clone()
 
-    print(" fovea step 3 ")
+    if pipe.debug:
+        print(" fovea step 3 ")
 
     if starters is not None:
         starters[3].record()
@@ -246,8 +274,8 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
         geom_rgb_precomp = parsed_geom_rgb,
         point_list_precomp = parsed_point_list,
         ranges_precomp = parsed_ranges,
-        tile_AMR_levels_last = parsed_tile_AMR_levels_step2,
-        tile_AMR_levels_current = parsed_tile_AMR_levels_step3,
+        # tile_AMR_levels_last = parsed_tile_AMR_levels_step2,
+        # tile_AMR_levels_current = parsed_tile_AMR_levels_step3,
         geomBuffer_precomp = geomBuffer2,
         binningBuffer_precomp = binningBuffer2,
         imageBuffer_precomp = imgBuffer2
@@ -263,6 +291,141 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
     # Those Gaussians that were frustum culled or had a radius of 0 were not visible.
     # They will be excluded from value updates used in the splitting criteria.
     return {"render": rendered_image3,
+            "viewspace_points": screenspace_points,
+            "visibility_filter" : radii > 0,
+            "radii": radii,
+            "means2D": parsed_means2D,
+            "conic_opacity": parsed_conic_opacity,
+            "geom_rgb": parsed_geom_rgb,
+            "point_list": parsed_point_list,
+            "ranges": parsed_ranges,
+            "tile_AMR_levels": parsed_tile_AMR_levels
+            }
+
+
+# render the entire scene once without foveation
+def render_once(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, scaling_modifier = 1.0, override_color = None, 
+           starter=None,ender=None):
+    """
+    Render the scene. 
+    
+    Background tensor (bg_color) must be on GPU!
+    """
+ 
+    # Create zero tensor. We will use it to make pytorch return gradients of the 2D (screen-space) means
+    screenspace_points = torch.zeros_like(pc.get_xyz, dtype=pc.get_xyz.dtype, requires_grad=True, device="cuda") + 0
+    try:
+        screenspace_points.retain_grad()
+    except:
+        pass
+
+    # Set up rasterization configuration
+    tanfovx = math.tan(viewpoint_camera.FoVx * 0.5)
+    tanfovy = math.tan(viewpoint_camera.FoVy * 0.5)
+
+    raster_settings = GaussianRasterizationSettings(
+        image_height=int(viewpoint_camera.image_height),
+        image_width=int(viewpoint_camera.image_width),
+        tanfovx=tanfovx,
+        tanfovy=tanfovy,
+        bg=bg_color,
+        scale_modifier=scaling_modifier,
+        viewmatrix=viewpoint_camera.world_view_transform,
+        projmatrix=viewpoint_camera.full_proj_transform,
+        sh_degree=pc.active_sh_degree,
+        campos=viewpoint_camera.camera_center,
+        prefiltered=False,
+        debug=pipe.debug
+    )
+
+    # print(raster_settings.debug)
+
+    rasterizer = GaussianRasterizer(raster_settings=raster_settings)
+
+    means3D = pc.get_xyz
+    means2D = screenspace_points
+    opacity = pc.get_opacity
+
+    # if starter is not None:
+    #     starter.record()
+
+    # If precomputed 3d covariance is provided, use it. If not, then it will be computed from
+    # scaling / rotation by the rasterizer.
+    scales = None
+    rotations = None
+    cov3D_precomp = None
+    if pipe.compute_cov3D_python:
+        cov3D_precomp = pc.get_covariance(scaling_modifier)
+    else:
+        scales = pc.get_scaling
+        rotations = pc.get_rotation
+
+    # If precomputed colors are provided, use them. Otherwise, if it is desired to precompute colors
+    # from SHs in Python, do it. If not, then SH -> RGB conversion will be done by rasterizer.
+    shs = None
+    colors_precomp = None
+    if override_color is None:
+        if pipe.convert_SHs_python:
+            shs_view = pc.get_features.transpose(1, 2).view(-1, 3, (pc.max_sh_degree+1)**2)
+            dir_pp = (pc.get_xyz - viewpoint_camera.camera_center.repeat(pc.get_features.shape[0], 1))
+            dir_pp_normalized = dir_pp/dir_pp.norm(dim=1, keepdim=True)
+            sh2rgb = eval_sh(pc.active_sh_degree, shs_view, dir_pp_normalized)
+            colors_precomp = torch.clamp_min(sh2rgb + 0.5, 0.0)
+        else:
+            shs = pc.get_features
+    else:
+        colors_precomp = override_color
+    
+    # I record times here because the color and cov are not computed in python when testing fps
+    # loading gaussian model parameters in python took similar time as rendering 
+    # this is also what fov-3DGS does: Fov-3DGS/fov3dgs/gaussian_renderer_fov_mmfr/__init__.py:72
+
+
+
+    # radii = torch.empty(0, dtype=torch.int32, device='cuda')
+    # parsed_point_list = torch.empty(0, dtype=torch.int32, device='cuda')
+    # parsed_ranges = torch.empty(0, dtype=torch.int32, device='cuda')
+    # parsed_tile_AMR_levels = torch.empty(0, dtype=torch.int32, device='cuda')
+
+    
+    if starter is not None:
+        starter.record()
+
+    # Rasterize visible Gaussians to image, obtain their radii (on screen). 
+    rendered_image, radii, parsed_means2D, parsed_conic_opacity, parsed_geom_rgb, parsed_point_list, parsed_ranges, parsed_tile_AMR_levels, _, _, _= rasterizer(
+        means3D = means3D,
+        means2D = means2D,
+        shs = shs,
+        colors_precomp = colors_precomp,
+        opacities = opacity,
+        scales = scales,
+        rotations = rotations,
+        cov3D_precomp = cov3D_precomp,
+        foveaStep = int(-2),
+        out_color_precomp = None,
+        radii_precomp = None,
+        means2D_precomp = None,
+        conic_opacity_precomp = None,
+        geom_rgb_precomp = None,
+        point_list_precomp = None,
+        ranges_precomp = None,
+        tile_AMR_levels_last = None,
+        tile_AMR_levels_current = None,
+        geomBuffer_precomp = None,
+        binningBuffer_precomp = None,
+        imageBuffer_precomp = None
+    )
+        
+    if ender is not None:
+        ender.record()
+
+
+    
+
+
+    # Those Gaussians that were frustum culled or had a radius of 0 were not visible.
+    # They will be excluded from value updates used in the splitting criteria.
+    return {"render": rendered_image,
             "viewspace_points": screenspace_points,
             "visibility_filter" : radii > 0,
             "radii": radii,

submodules/diff-gaussian-rasterization-amr/diff_gaussian_rasterization_amr/__init__.py
@@ -131,17 +131,42 @@ class _RasterizeGaussians(torch.autograd.Function):
             raster_settings.debug
         )
 
-        # Invoke C++/CUDA rasterizer
-        if raster_settings.debug:
-            cpu_args = cpu_deep_copy_tuple(args) # Copy them before they can be corrupted
-            try:
-                num_rendered, color, radii, parsed_means2D, parsed_conic_opacity, parsed_geom_rgb, parsed_point_list, parsed_ranges, parsed_tile_AMR_levels, geomBuffer, binningBuffer, imgBuffer= _C.rasterize_gaussians(*args)
-            except Exception as ex:
-                torch.save(cpu_args, "snapshot_fw.dump")
-                print("\nAn error occured in forward. Please forward snapshot_fw.dump for debugging.")
-                raise ex
-        else:
-            num_rendered, color, radii, parsed_means2D, parsed_conic_opacity, parsed_geom_rgb, parsed_point_list, parsed_ranges, parsed_tile_AMR_levels, geomBuffer, binningBuffer, imgBuffer= _C.rasterize_gaussians(*args)
+        # turn off the original debug for now
+
+        # # Invoke C++/CUDA rasterizer
+        # if raster_settings.debug:
+        #     cpu_args = cpu_deep_copy_tuple(args) # Copy them before they can be corrupted
+        #     try:
+        #         num_rendered, color, radii, parsed_means2D, parsed_conic_opacity, parsed_geom_rgb, parsed_point_list, parsed_ranges, parsed_tile_AMR_levels, geomBuffer, binningBuffer, imgBuffer= _C.rasterize_gaussians(*args)
+        #     except Exception as ex:
+        #         torch.save(cpu_args, "snapshot_fw.dump")
+        #         print("\nAn error occured in forward. Please forward snapshot_fw.dump for debugging.")
+        #         raise ex
+        # else:
+        #     num_rendered, color, radii, parsed_means2D, parsed_conic_opacity, parsed_geom_rgb, parsed_point_list, parsed_ranges, parsed_tile_AMR_levels, geomBuffer, binningBuffer, imgBuffer= _C.rasterize_gaussians(*args)
+        
+        
+        num_rendered, color, radii, parsed_means2D, parsed_conic_opacity, parsed_geom_rgb, parsed_point_list, parsed_ranges, parsed_tile_AMR_levels, geomBuffer, binningBuffer, imgBuffer= _C.rasterize_gaussians(*args)
+
+
+
+        # # Ensure correct types without conversion
+        # if raster_settings.debug:
+        #     # assert radii.dtype == torch.int32
+        #     # assert parsed_point_list.dtype == torch.int32
+        #     # assert parsed_ranges.dtype == torch.int32
+        #     # assert parsed_tile_AMR_levels.dtype == torch.int32
+        #     # assert geomBuffer.dtype == torch.int8
+        #     # assert binningBuffer.dtype == torch.int8
+        #     # assert imgBuffer.dtype == torch.int8
+        #     # print the dtype
+        #     print("radii.dtype: ", radii.dtype)
+        #     print("parsed_point_list.dtype: ", parsed_point_list.dtype)
+        #     print("parsed_ranges.dtype: ", parsed_ranges.dtype)
+        #     print("parsed_tile_AMR_levels.dtype: ", parsed_tile_AMR_levels.dtype)
+        #     print("geomBuffer.dtype: ", geomBuffer.dtype)
+        #     print("binningBuffer.dtype: ", binningBuffer.dtype)
+        #     print("imgBuffer.dtype: ", imgBuffer.dtype)
 
         # Keep relevant tensors for backward
         ctx.raster_settings = raster_settings
@@ -237,7 +262,7 @@ class GaussianRasterizer(nn.Module):
         return visible
 
     def forward(self, means3D, means2D, opacities, shs = None, colors_precomp = None, scales = None, rotations = None, cov3D_precomp = None,
-                foveaStep = 0,
+                foveaStep = int(0),
                 out_color_precomp = None,
                 radii_precomp = None,
                 means2D_precomp = None,
@@ -270,11 +295,11 @@ class GaussianRasterizer(nn.Module):
             rotations = torch.Tensor([])
         if cov3D_precomp is None:
             cov3D_precomp = torch.Tensor([])
-        
+
         if out_color_precomp is None:
             out_color_precomp = torch.Tensor([])
         if radii_precomp is None:
-            radii_precomp = torch.Tensor([])
+            radii_precomp = torch.Tensor([]).to(torch.int32)
         if means2D_precomp is None:
             means2D_precomp = torch.Tensor([])
         if conic_opacity_precomp is None:
@@ -282,31 +307,31 @@ class GaussianRasterizer(nn.Module):
         if geom_rgb_precomp is None:
             geom_rgb_precomp = torch.Tensor([])
         if point_list_precomp is None:
-            point_list_precomp = torch.Tensor([])
+            point_list_precomp = torch.Tensor([]).to(torch.int32)
         if ranges_precomp is None:
-            ranges_precomp = torch.Tensor([])
+            ranges_precomp = torch.Tensor([]).to(torch.int32)
         if tile_AMR_levels_last is None:
-            tile_AMR_levels_last = torch.Tensor([])
+            tile_AMR_levels_last = torch.Tensor([]).to(torch.int32)
         if tile_AMR_levels_current is None:
-            tile_AMR_levels_current = torch.Tensor([])
+            tile_AMR_levels_current = torch.Tensor([]).to(torch.int32)
         if geomBuffer_precomp is None:
-            geomBuffer_precomp = torch.Tensor([])
+            geomBuffer_precomp = torch.Tensor([]).to(torch.uint8)
         if binningBuffer_precomp is None:
-            binningBuffer_precomp = torch.Tensor([])
+            binningBuffer_precomp = torch.Tensor([]).to(torch.uint8)
         if imageBuffer_precomp is None:
-            imageBuffer_precomp = torch.Tensor([])
+            imageBuffer_precomp = torch.Tensor([]).to(torch.uint8)
+            # set the buffer type to char (int8?)
+        #     geomBuffer_precomp = geomBuffer_precomp.to(torch.int8)
+        #     binningBuffer_precomp = binningBuffer_precomp.to(torch.int8)
+        #     imageBuffer_precomp = imageBuffer_precomp.to(torch.int8)
 
-        foveaStep = int(foveaStep)
-        radii_precomp = radii_precomp.to(torch.int32)
-        point_list_precomp = point_list_precomp.to(torch.int32)
-        ranges_precomp = ranges_precomp.to(torch.int32)
-        tile_AMR_levels_last = tile_AMR_levels_last.to(torch.int32)
-        tile_AMR_levels_current = tile_AMR_levels_current.to(torch.int32)
+        # foveaStep = int(foveaStep)
+        # radii_precomp = radii_precomp.to(torch.int32)
+        # point_list_precomp = point_list_precomp.to(torch.int32)
+        # ranges_precomp = ranges_precomp.to(torch.int32)
+        # tile_AMR_levels_last = tile_AMR_levels_last.to(torch.int32)
+        # tile_AMR_levels_current = tile_AMR_levels_current.to(torch.int32)
 
-        # set the buffer type to char (int8?)
-        geomBuffer_precomp = geomBuffer_precomp.to(torch.int8)
-        binningBuffer_precomp = binningBuffer_precomp.to(torch.int8)
-        imageBuffer_precomp = imageBuffer_precomp.to(torch.int8)
 
         # Invoke C++/CUDA rasterization routine
         return rasterize_gaussians(